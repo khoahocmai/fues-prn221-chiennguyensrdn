7728acebe141d2da4502d57744c1f7d87259467c
KhoaHocMai113 <doduongdangkhoa1103@gmail.com>
2024-06-25 10:43:36 +0700

    Update structure DB

--- BusinnessObjects/ExchangeRequest.cs
+++ BusinnessObjects/ExchangeRequest.cs
@@ -6,7 +6,6 @@ namespace BusinnessObjects
     public partial class ExchangeRequest
     {
         public int Id { get; set; }
-        public int? OwnProductId { get; set; }
         public int? ProductId { get; set; }
         public int? RequesterId { get; set; }
         public string? Message { get; set; }
@@ -14,7 +13,6 @@ namespace BusinnessObjects
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
-        public virtual Product? OwnProduct { get; set; }
         public virtual Product? Product { get; set; }
         public virtual User? Requester { get; set; }
     }
--- BusinnessObjects/Report.cs
+++ BusinnessObjects/Report.cs
@@ -7,14 +7,12 @@ namespace BusinnessObjects
     {
         public int Id { get; set; }
         public int? ReporterId { get; set; }
-        public int? ReportedId { get; set; }
         public int? ProductId { get; set; }
         public string Status { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
         public virtual Product? Product { get; set; }
-        public virtual User? Reported { get; set; }
         public virtual User? Reporter { get; set; }
     }
 }
