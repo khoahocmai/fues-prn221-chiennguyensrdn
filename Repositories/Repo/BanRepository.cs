69bf580cbd207293a441e7bdb306311ef96b7bcb
KhoaHocMai113 <doduongdangkhoa1103@gmail.com>
2024-07-02 19:13:49 +0700

    Ban and Exchange request CRUD

---
+++ Repositories/Repo/BanRepository.cs
@@ -0,0 +1,26 @@
+﻿using BusinessObjects;
+using DataAccessObjects.DAO;
+using Repositories.IRepo;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repositories.Repositories
+{
+    public class BanRepository : IBanRepository
+    {
+        public Task<List<Ban>> GetBans()
+            => BanDAO.Instance.GetBans();
+
+        public Task<Ban> GetBanById(int id)
+            => BanDAO.Instance.GetBanById(id);
+
+        public Task AddBan(Ban ban)
+            => BanDAO.Instance.AddBan(ban);
+
+        public Task UpdateBan(Ban ban)
+            => BanDAO.Instance.UpdateBan(ban);
+
+        public Task RemoveBan(int id)
+            => BanDAO.Instance.RemoveBan(id);
+    }
+}
